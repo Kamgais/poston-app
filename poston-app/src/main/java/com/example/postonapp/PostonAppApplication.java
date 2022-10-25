e15e2e34bae3e6c8368857b5f5f9945c9cf35726
Cyril Kamgais <102966559+Kamgais@users.noreply.github.com>
2022-10-25 16:17:38 +0200

    first initialize

---
+++ poston-app/src/main/java/com/example/postonapp/PostonAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.postonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostonAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostonAppApplication.class, args);
+    }
+
+}
